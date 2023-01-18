45bb5e1fec0d1faee40ca34c965311133aebd0e4
lukablaskovic <lukablaskovic2000@gmail.com>
2023-01-18 14:20:36 +0100

    installed tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
@@ -11,6 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
   <HelloWorld msg="Vite + Vue" />
 </template>
 
